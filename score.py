9b986e7813101e9af2738980a8ee626e767c4476
Ben Albrinck <ben.albrinck@gmail.com>
2022-02-19 17:31:54 -0500

    Removed perfect probability and added expected score

--- score.py
+++ score.py
@@ -1,8 +1,4 @@
-
-# Probability: literally just all the things multiplied together
-# Expected value: I think it is the prob that the matchup happens * prob that it picked correctly
-#   * the points you would get for that round all added up
-
+import collections
 import defy_logging
 import defy_model
 import json
@@ -56,23 +52,45 @@ def simulate_game(team1, team2):
 	# Get probabilities
 	result = model(normal_game_input).numpy()
 
-	return result[0]
+	return {
+		team1: result[0][0],
+		team2: result[0][1]
+	}
+
+
+def simulate_combinations(first_teams, second_teams):
+	first_team_keys = list(first_teams.keys())
+	second_team_keys = list(second_teams.keys())
+	results = {}
+
+	for f in first_team_keys:
+		if f not in results:
+			results[f] = 0
+
+		for s in second_team_keys:
+			if s not in results:
+				results[s] = 0
+
+			result_prob = simulate_game(f, s)
+			result_prob = {k: result_prob[k] * first_teams[f] * second_teams[s] for k in result_prob}
+			
+			results[f] += result_prob[f]
+			results[s] += result_prob[s]
+	
+	return results
 
 
 def simulate_round(teams, correct_teams):
-    """Simulate each game in the round."""
-    result_probs = []
+	"""Simulate each game in the round."""
+	results = []
+	result_probs = []
 
-    for game in range(int(len(teams) / 2)):
-        result_prob = simulate_game(teams[game * 2], teams[(game * 2) + 1])
+	for game in range(int(len(teams) / 2)):
+		result = simulate_combinations(teams[game * 2], teams[(game * 2) + 1])
+		results.append(result)
+		result_probs.append(result[correct_teams[game]])
 
-        if teams[game * 2] == correct_teams[game]:
-            # First team wins
-            result_probs.append(result_prob[0])
-        else:
-            result_probs.append(result_prob[1])
-
-    return result_probs
+	return results, result_probs
 
 
 def display_results(round_prob):
@@ -112,8 +130,14 @@ if __name__ == '__main__':
 	with open('setup/tournament_teams.txt') as file:
 		split_text = file.read().split('\n\n')
 	
-	first_four_teams = split_text[0].split('\n')
-	tournament_string = split_text[1] + ''
+	first_four_teams = []
+	tournament_teams = []
+
+	for t in split_text[0].split('\n'):
+		first_four_teams.append({ t: 1 })
+	
+	for t in split_text[1].split('\n'):
+		tournament_teams.append({ t: 1 })
 
 	# Get normalization value
 	npz_file = np.load(f'data/data_{year}.npz', allow_pickle=True)
@@ -134,41 +158,35 @@ if __name__ == '__main__':
 
 	# First Four
 	logger.info('First Four:')
-	first_four_prob = simulate_round(first_four_teams, true_results[0])
+	first_four_results, first_four_prob = simulate_round(first_four_teams, true_results[0])
 	display_results(first_four_prob)
 	results_string += '\n'.join([str(p) for p in first_four_prob]) + '\n\n'
-	first_four_split = true_results[0]
 
 	# Rest of tournament
 	# Insert first four into the main tournament
 	expected_score = 0
-	tournament_teams = tournament_string.format(*first_four_split).split('\n')
-	tournament_probs = []
-
 	first_four_index = 0
-	for t in tournament_string.split('\n'):
-		if t == '{}':
-			tournament_probs.append(first_four_prob[first_four_index])
+
+	for i, t in enumerate(tournament_teams):
+		if list(t.keys())[0] == '{}':
+			tournament_teams[i] = first_four_results[first_four_index]
 			first_four_index += 1
-		else:
-			tournament_probs.append(1)
 
 	# Simulate main tournament
 	for tournament_round in range(6):
 		logger.info(f'Round {tournament_round + 1}:')
-		round_prob = simulate_round(tournament_teams, true_results[tournament_round + 1])
+		round_results, round_prob = simulate_round(tournament_teams, true_results[tournament_round + 1])
 		display_results(round_prob)
 
 		# Calculate running probabilities and expected score
-		tournament_probs = [round_prob[p] * (tournament_probs[p * 2] * tournament_probs[(p * 2) + 1]) for p in range(len(round_prob))]
-		expected_score += sum([p * ((2 ** tournament_round) * 10) for p in tournament_probs])
+		expected_score += sum([p * ((2 ** tournament_round) * 10) for p in round_prob])
 		
 		# Record results and repeat
 		results_string += '\n'.join([str(p) for p in round_prob]) + '\n\n'
-		tournament_teams = true_results[tournament_round + 1]
+		tournament_teams = round_results[:]
 
-	logger.info(f'Probability of picking perfectly: {tournament_probs[0] * 100}%')
 	logger.info(f'Expected score: {expected_score}')
+	logger.info(tournament_teams)
 
 	results_file_name = checkpoint_path.replace('networks/', '')[:-5]
 
